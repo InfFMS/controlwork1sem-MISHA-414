6b5587a802a1a1893b94042b093f10209df8aaeb
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-12-04 14:25:31 +0700

    task11

--- task11.py
+++ task11.py
@@ -1,12 +1,15 @@
 def delit(n):
     count=0
+    answer=[]
     for i in range(2, n):
         if n%i==0:
             count+=1
-    return count
+            answer.append(i)
+    return [count, answer]
 
 cou=0
 for num in range(174457, 174506):
     d=delit(num)
-    if d==2:
-        print(num)
+    if d[0]==2:
+        print(num, d[1])
+
